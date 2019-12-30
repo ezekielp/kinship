772cebe50c61618f9a85d39515f2f0e67eed0f8a
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-30 11:36:02 -0500

    Create app.jsx and landing_page.jsx

---
+++ frontend/src/components/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { AuthRoute, ProtectedRoute } from '../util/route_util';
+import { Switch } from 'react-router-dom';
+
+import LandingPage from './landing_page';
+
+const App = () => (
+    <Switch>
+        <AuthRoute exact path="/" component={LandingPage} />
+    </Switch>
+);
+
+export default App;
