a9407a27bd189a288859cf710d45161633e4dfc7
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-30 11:14:38 -0500

    Create root reducer and session reducer

---
+++ frontend/src/reducers/session_reducer.js
@@ -0,0 +1,21 @@
+import { RECEIVE_USER_LOGOUT } from '../actions/session_actions';
+
+const initialState = {
+    isAuthenticated: false,
+    user: {}
+};
+
+const sessionReducer = (state = initialState, action) => {
+    Object.freeze(state);
+    switch(action.type) {
+        case RECEIVE_USER_LOGOUT:
+            return {
+              isAuthenticated: false,
+              user: undefined
+            };
+        default:
+            return state;
+    }
+}
+
+export default sessionReducer;
