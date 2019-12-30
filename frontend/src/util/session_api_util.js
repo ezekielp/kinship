ed96fc226a5262eda75dad343f2810b7e0f74d08
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-30 10:53:35 -0500

    Initial commit

---
+++ frontend/src/util/session_api_util.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+export const setAuthToken = token => {
+    if (token) {
+        axios.defaults.headers.common['Authorization'] = token;
+    } else {
+        delete axios.defaults.headers.common['Authorization'];
+    }
+};
+
