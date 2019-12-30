c6ffde5a375d29d08c2c122a3e95593ea4d149f1
Kzhao <ken.zh77@gmail.com>
2019-12-30 15:29:33 -0500

    Add friends util and actions

---
+++ frontend/src/reducers/friends_reducer.js
@@ -0,0 +1,2 @@
+import { RECEIVE_ALL_FRIENDS, RECEIVE_FRIEND, REMOVE_FRIEND} from '../actions/friends_actions';
+
---
+++ frontend/src/util/friends_api_util.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+/*
+axios requests to friends for:
+
+1. fetchFriends
+2. fetchFriend
+3. createFriend
+4. updateFriend
+5. deleteFriend
+*/
+
+
+export const fetchFriends = (id) => {
+    return axios.get(`/api/friends/user/${id}`)
+}
+
+export const fetchFriend = (id) => {
+    return axios.get(`/api/friends/${id}`)
+}
+
+export const createFriend = (data) => {
+    return axios.post('/api/friends', data)
+}
+
+export const updateFriend = (data) => {
+    return axios.patch(`/api/friends/${data.id}`, data)
+}
+
+export const deleteFriend = (id) => {
+    return axios.delete(`/api/friends/${id}`)
+}
+
+
