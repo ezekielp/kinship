36c9774d53a3bede7d2c407b2fc42f6f70cf169c
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-31 11:16:42 -0500

    Finish basic skeleton and preliminary styling of friends index page

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -10,12 +10,16 @@ class Navbar extends React.Component {
     render() {
         return (
           <div className="navbar-container">
-              <Link to="/friends" className="navbar-logo-link">
-                <div className="navbar-logo-container">
+            <Link to="/friends" className="navbar-logo-link">
+              <div className="navbar-logo-container">
                 <div className="navbar-logo-first-F">F</div>
                 <div className="navbar-logo-second-F">F</div>
+              </div>
+            </Link>
+            <div
+                className="navbar-logout-btn" 
+                onClick={() => this.props.logout()}>Logout
                 </div>
-              </Link>
           </div>
         );
     }
--- frontend/src/components/nav/navbar_container.js
+++ frontend/src/components/nav/navbar_container.js
@@ -1,15 +1,16 @@
 import { connect } from 'react-redux';
+import { logout } from '../../actions/session_actions';
 import Navbar from './navbar';
 
 const msp = state => {
     return {
-        
+        loggedIn: state.session.isAuthenticated
     }
 }
 
 const mdp = dispatch => {
     return {
-
+        logout: () => dispatch(logout())
     }
 }
 
