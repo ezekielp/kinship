4957943942cc0f9bec68d0575b16f6dc71595f7b
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-31 15:15:57 -0500

    Create skeleton for friend show page

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -4,11 +4,13 @@ import { Route, Switch } from 'react-router-dom';
 
 import LandingPage from './landing_page/landing_page';
 import FriendsIndex from './friends/friends_index';
+import FriendShowContainer from './friends/friend_show_container';
 
 const App = () => (
     <Switch>
         <Route exact path="/friends" component={FriendsIndex} />
         {/* <ProtectedRoute exact path="/friends" component={FriendsIndex} /> */}
+        <Route exact path="/friends/dummy_show" component={FriendShowContainer} />
         <AuthRoute path="/" component={LandingPage} />
     </Switch>
 );
