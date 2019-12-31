7ad8cd9872592586c70915ccfb9b645c7b518b23
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-31 12:14:12 -0500

    Adjust logout logic

--- frontend/src/components/nav/navbar.jsx
+++ frontend/src/components/nav/navbar.jsx
@@ -1,13 +1,36 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import "./navbar.css";
 
 class Navbar extends React.Component {
     constructor(props) {
         super(props);
+
+        this.state = {
+          // justLoggedOut: false,
+          loggedIn: this.props.loggedIn
+        }
+
+        
+    }
+
+    handleLogout() {
+      this.props.logout();
+      this.setState({
+        loggedIn: false
+      });
     }
 
     render() {
+      // if (this.state.justLoggedOut) {
+      //   this.setState({ justLoggedOut: false });
+      //   return <Redirect to="/" />;
+      // }
+      
+      if (!this.state.loggedIn) {
+        return <Redirect to="/" />;
+      }
+
         return (
           <div className="navbar-container">
             <Link to="/friends" className="navbar-logo-link">
