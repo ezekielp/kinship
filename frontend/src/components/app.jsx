e7adec1590d65ea50bafcfbbccc182325f77a724
Ezekiel Pfeifer <ezekielp@gmail.com>
2019-12-31 09:26:26 -0500

    Adjust route_util and app to route to main user home page (friends)

--- frontend/src/components/app.jsx
+++ frontend/src/components/app.jsx
@@ -3,10 +3,12 @@ import { AuthRoute, ProtectedRoute } from '../util/route_util';
 import { Switch } from 'react-router-dom';
 
 import LandingPage from './landing_page/landing_page';
+import FriendsIndex from './friends/friends_index';
 
 const App = () => (
     <Switch>
-        <AuthRoute exact path="/" component={LandingPage} />
+        <ProtectedRoute exact path="/friends" component={FriendsIndex} />
+        <AuthRoute path="/" component={LandingPage} />
     </Switch>
 );
 
