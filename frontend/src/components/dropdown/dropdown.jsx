6f2f7e6309e2361f2b06724271fa74efeaf93876
taeinha <taeinha@gmail.com>
2020-01-01 15:39:28 -0500

    Update dropdown presentational component

--- frontend/src/components/dropdown/dropdown.jsx
+++ frontend/src/components/dropdown/dropdown.jsx
@@ -1,10 +1,57 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './dropdown.css';
 
 export default class dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showDropdown: false
+    };
+    this.showDropdown = this.showDropdown.bind(this);
+    this.hideDropdown = this.hideDropdown.bind(this);
+    this.toggleDropdown = this.toggleDropdown.bind(this);
+  }
+
+  showDropdown() {
+    this.setState({ showDropdown: true });
+  }
+
+  hideDropdown() {
+    this.setState({ showDropdown: false });
+  }
+
+  toggleDropdown() {
+    this.setState({ showDropdown: !this.state.showDropdown });
+  }
+
+  _dropdownList() {
+    const { arr } = this.props;
+    if (this.state.showDropdown) {
+      const items = arr.map((el, i) => {
+        <section key={i}>
+          {el}
+        </section>
+      });
+
+      return (
+        <div className="dropdown-items-container">
+          {items}
+        </div>
+      )
+    }
+    
+  }
+
   render() {
     return (
-      <div>
-        
+      <div
+        className="dropdown-container"
+        onBlur={() => this.hideDropdown()}
+        onFocus={() => this.showDropdown()}
+        tabIndex="0">
+      >
+        <header>Add a field</header>
+        {this._dropdownList()}
       </div>
     )
   }
