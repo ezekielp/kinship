16a0e408d3bc95d8348132732c5098cae8ce0e98
Ezekiel Pfeifer <ezekielp@gmail.com>
2020-01-02 10:55:18 -0500

    Begin work on birthdays for friends index sidebar

--- frontend/src/components/friends/friends_sidebar.jsx
+++ frontend/src/components/friends/friends_sidebar.jsx
@@ -6,12 +6,17 @@ class FriendsSidebar extends React.Component {
         super(props);
     }
 
+    renderUpcomingBirthdays() {
+        
+    }
+
     render() {
 
 
         return (
             <div className="friends-sidebar-container">
                 <h2 className="friends-sidebar-header">Dashboard</h2>
+                <h3 className="friends-sidebar-birthdays-header">Upcoming birthdays</h3>
             </div>
         )
     }
